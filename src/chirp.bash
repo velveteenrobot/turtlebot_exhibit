cb951e1d56a2ba5c1b5859dc7a263da1fe4872dd
Sarah Elliott <sksellio@gmail.com>
2012-07-20 21:55:33 -0400

    Compiles under new name.

--- src/chirp.bash
+++ src/chirp.bash
@@ -5,4 +5,4 @@ amixer -c 0 sset PCM,0 100%
 amixer -c 0 sset Master,0 100%
 
 # Then moo.
-aplay /opt/ros/electric/stacks/turtlebot_apps/turtlebot_follower/src/cow.wav
+aplay /opt/ros/electric/stacks/turtlebot_apps/turtlebot_exhibit/src/cow.wav
